c16a547e4bfa90569192dbb14a6932188126339c
Jessi <jessi_gao@163.com>
2025-05-22 12:03:01 +0200

    prefix folder and sdk

--- s3/prefixes/python_sdk.py
+++ s3/prefixes/python_sdk.py
@@ -1,10 +1,10 @@
-import boto3
+# import boto3
 
-s3 = boto3.client('s3')
-bucket = 'prefixes-5432'
-prefix = 'hello/'  # folder path
+# s3 = boto3.client('s3')
+# bucket = 'prefixes-5432'
+# prefix = 'hello/'  # folder path
 
-response = s3.list_objects_v2(Bucket=bucket, Prefix=prefix)
+# response = s3.list_objects_v2(Bucket=bucket, Prefix=prefix)
 
-for obj in response.get('Contents', []):
-    print(obj['Key'])
+# for obj in response.get('Contents', []):
+#     print(obj['Key'])
